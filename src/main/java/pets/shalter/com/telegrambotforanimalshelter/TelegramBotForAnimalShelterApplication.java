10843d99299f21379016645ffcf888f8fb352a37
DiannaaaMm <murlaevadiana@gmail.com>
2024-12-03 19:25:44 +0300

    init project

---
+++ src/main/java/pets/shalter/com/telegrambotforanimalshelter/TelegramBotForAnimalShelterApplication.java
@@ -0,0 +1,14 @@
+package pets.shalter.com.telegrambotforanimalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TelegramBotForAnimalShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotForAnimalShelterApplication.class, args);
+	}
+
+}
